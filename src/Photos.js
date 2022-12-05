39663ec2aa0b3e8f982fe7f04ba6d0e15ae0f0e1
gguilhermelopes <guilhermelopes275@gmail.com>
2022-12-05 16:01:48 -0300

    photo modal

--- src/Photos.js
+++ src/Photos.js
@@ -12,44 +12,47 @@ import PhotosModal from "./PhotosModal";
 
 const Photos = () => {
   const [photoModal, setPhotoModal] = React.useState(false);
+  const [photoMain, setPhotoMain] = React.useState(0);
+  const [active, setActive] = React.useState(photoMain);
 
-  function handleClickModal() {
+  const photos = [img1, img2, img3, img4];
+  const photosThumb = [img1Thumb, img2Thumb, img3Thumb, img4Thumb];
+
+  function handleClickModal({ currentTarget }) {
     setPhotoModal(true);
+    setPhotoMain(currentTarget.id);
+    setActive(currentTarget.id);
   }
 
   return (
     <section className={styles.photos}>
-      <img onClick={handleClickModal} src={img1} alt="Sneaker" />
-      {photoModal && <PhotosModal />}
+      {photoModal ? (
+        <img src={img1} alt="Sneaker" />
+      ) : (
+        <img src={photos[photoMain]} alt="Sneaker" />
+      )}
+      {photoModal && (
+        <PhotosModal
+          setActiveMain={setActive}
+          setPhotoModal={setPhotoModal}
+          setPhotoMain={setPhotoMain}
+          photoMain={photoMain}
+        />
+      )}
       <ul className={styles.photoList}>
-        <li>
-          <img
-            onClick={handleClickModal}
-            src={img1Thumb}
-            alt="Sneaker Thumbnail"
-          />
-        </li>
-        <li>
-          <img
-            onClick={handleClickModal}
-            src={img2Thumb}
-            alt="Sneaker Thumbnail"
-          />
-        </li>
-        <li>
-          <img
-            onClick={handleClickModal}
-            src={img3Thumb}
-            alt="Sneaker Thumbnail"
-          />
-        </li>
-        <li>
-          <img
-            onClick={handleClickModal}
-            src={img4Thumb}
-            alt="Sneaker Thumbnail"
-          />
-        </li>
+        {photosThumb.map((photo, index) => {
+          return (
+            <li key={photo}>
+              <img
+                className={+active === +index ? "active" : ""}
+                id={index}
+                src={photo}
+                alt="Sneaker Thumbnail"
+                onClick={handleClickModal}
+              />
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
--- src/PhotosModal.js
+++ src/PhotosModal.js
@@ -1,7 +1,87 @@
 import React from "react";
+import img1 from "./images/image-product-1.jpg";
+import img1Thumb from "./images/image-product-1-thumbnail.jpg";
+import img2 from "./images/image-product-2.jpg";
+import img2Thumb from "./images/image-product-2-thumbnail.jpg";
+import img3 from "./images/image-product-3.jpg";
+import img3Thumb from "./images/image-product-3-thumbnail.jpg";
+import img4 from "./images/image-product-4.jpg";
+import img4Thumb from "./images/image-product-4-thumbnail.jpg";
+import { ReactComponent as Close } from "./images/icon-close.svg";
+import { ReactComponent as Previous } from "./images/icon-previous.svg";
+import { ReactComponent as Next } from "./images/icon-next.svg";
+import styles from "./PhotosModal.module.css";
 
-const PhotosModal = () => {
-  return <div>PhotosModal</div>;
+const PhotosModal = ({
+  setPhotoModal,
+  photoMain,
+  setPhotoMain,
+  setActiveMain,
+}) => {
+  const [photo, setPhoto] = React.useState(photoMain);
+  const [active, setActive] = React.useState(photoMain);
+
+  const photos = [img1, img2, img3, img4];
+  const photosThumb = [img1Thumb, img2Thumb, img3Thumb, img4Thumb];
+
+  function handleOutsideClick({ target, currentTarget }) {
+    if (currentTarget === target) setPhotoModal(false);
+  }
+
+  function handlePhotoClick({ currentTarget }) {
+    setPhoto(currentTarget.id);
+    setPhotoMain(currentTarget.id);
+    setActive(currentTarget.id);
+    setActiveMain(currentTarget.id);
+  }
+
+  function handlePrev() {
+    if (photo > 0 && photoMain > 0) {
+      setPhoto(+photo - 1);
+      setPhotoMain(+photoMain - 1);
+      setActive(+active - 1);
+      setActiveMain(+active - 1);
+    }
+  }
+
+  function handleNext(event) {
+    event.preventDefault();
+    if (+active < photosThumb.length - 1) {
+      console.log(photosThumb.length - 1);
+      setPhoto(+photo + 1);
+      setPhotoMain(+photoMain + 1);
+      setActive(+active + 1);
+      setActiveMain(+active + 1);
+    }
+  }
+
+  return (
+    <section
+      onClick={handleOutsideClick}
+      className={`${styles.photosModalContainer}`}
+    >
+      <div>
+        <Close onClick={() => setPhotoModal(false)} className={styles.close} />
+        <Previous onClick={handlePrev} className={styles.previous} />
+        <img src={photos[photo]} alt={`Sneaker ${photo + 1}`} />
+        <Next onClick={handleNext} className={styles.next} />
+      </div>
+
+      <ul className={styles.photosModalList}>
+        {photosThumb.map((photo, index) => (
+          <li key={photo}>
+            <img
+              className={+active === +index ? "active" : ""}
+              id={index}
+              onClick={handlePhotoClick}
+              src={photo}
+              alt={`Sneaker ${index + 1}`}
+            />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default PhotosModal;
